1689ba17bc6b9a3d9bd266eb606e66f0ff8a8730
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2023-10-20 17:44:29 +0800

    Header Footer with react-bootstrap, custom bootstrap theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,19 @@
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 const App = () => {
   return (
-    <div>Welcome to Techshop</div>
+    <>
+      
+      <Header />
+      <main className="py-3">
+        <Container>
+        <h1>Welcome to Techno</h1>  
+        </Container>
+      </main>
+      <Footer/>s
+    </>
   )
 }
 

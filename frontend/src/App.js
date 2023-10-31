1c3fa64cb6e88044d5a0a4275e57f1d558ea59de
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2023-10-31 11:57:26 +0800

    React-Router-Dom and Link with bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { Container } from "react-bootstrap"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import HomeScreen from "./screens/HomeScreen"
+import { Outlet } from "react-router-dom"
+
 
 const App = () => {
   return (
@@ -10,6 +12,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
+          <Outlet/>
           <HomeScreen/>  
         </Container>
       </main>
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -9,13 +9,13 @@ const HomeScreen = () => {
           <h1>Latest Products</h1>
           <Row>
               {products.map((product) => (
-                  <Col key={product.id}  sm={12} md={6} lg={4} xl={3}>
+                  <Col key={product.id}   sm={12} md={6} lg={4} xl={3}>
                       <h3>{product.name}</h3>
                       <Product product={product}/>
                   </Col>
               ))}
           </Row>
-      </>
+      </> 
   )
 }
 

fd1b3e5348315368e77863a1bb46101a88fa9e7b
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2024-05-24 16:28:04 +0800

    env, fetch products

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
 import {Link, useParams} from 'react-router-dom'
-import products from "../products"
 import {Row,Col,Image,ListGroup,Card,Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
+import axios from 'axios'
+
 
 const ProductScreen = () => {
     const { id: productId } = useParams()
-    const product = products.find((p) => p._id == productId)
+    const [product,setProduct] = useState([])
+    
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const {data} = await axios.get('/api/products/'+productId)
+            setProduct(data)
+        }
+        fetchProduct()
+    },[productId])
     
   return (
       <>

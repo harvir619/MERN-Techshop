3661a1ca61038f7969cfe5a775ab9574f4073743
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2024-06-13 16:21:48 +0800

    Redux, Loader, Message
    Redux, get product details, redux store , api slice, products, api slice and get products engpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import { Container } from "react-bootstrap"
 import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import HomeScreen from "./screens/HomeScreen"
 
 
 const App = () => {
@@ -13,7 +12,6 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Outlet/>
-          <HomeScreen/>  
         </Container>
       </main>
       <Footer/>

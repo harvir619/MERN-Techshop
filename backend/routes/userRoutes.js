a6a9f1fa7f768e3bc20a7cb25d5d86d599a732f9
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2024-08-26 17:59:22 +0800

    seting up the usercontroller,route,serverjs

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+    authUser, getUserProfile, registerUser,
+    updateUserProfile, getUsers,
+    deleteUser, getUserById, updateUser,
+    logoutUser
+ } from "../controllers/userController.js";
+// import { protect, admin } from "../middleware/authMiddleware.js";
+const router = express.Router();
+
+
+router.route('/').post(registerUser).get( getUsers)
+router.post('/logout', logoutUser)
+router.post('/login', authUser)
+router.route('/profile').get( getUserProfile).put( updateUserProfile)
+router.route('./:id').delete( deleteUser).get( getUserById).put( updateUser)
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 dotenv.config()
 import express from 'express'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
@@ -16,6 +17,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)

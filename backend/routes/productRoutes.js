a220bc58e5c5e68e22edfb07242b6de66291e42b
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2024-09-16 18:12:20 +0800

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,5 +6,5 @@ import { protect, admin } from "../middleware/authMiddleware.js";
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/top').get(getTopProducts)
 router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct)
-router.route('/:id/reviews').post(protect, admin, createProductReview)
+router.route('/:id/reviews').post(protect, createProductReview)
 export default router;
